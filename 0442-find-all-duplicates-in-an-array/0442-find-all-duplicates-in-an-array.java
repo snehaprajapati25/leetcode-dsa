51b6078ff1d7ac3ec0088ce7f809bf0a1c216022
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-01-14 13:21:38 +0530

    Time: 6 ms (90.06%), Space: 57.7 MB (60.07%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -5,7 +5,7 @@ class Solution {
         // approach
         // For each number x, go to index x - 1
         // If value at that index is negative, x is a duplicate
-        // Otherwise, make it negative to mark visited
+        // Otherwise, make it negative to mark visited  TC- O(n), SC- O(1)
 
         for(int i=0; i<nums.length; i++){
             int idx = Math.abs(nums[i]) - 1;
