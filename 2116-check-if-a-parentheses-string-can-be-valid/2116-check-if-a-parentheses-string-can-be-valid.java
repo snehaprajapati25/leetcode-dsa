1bbb32ffd73599c420b683b36600811c8a35b4b7
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-01-15 13:26:04 +0530

    Time: 15 ms (72.99%), Space: 47.8 MB (72.35%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean canBeValid(String s, String locked) {
+// We use two greedy passes. In the first pass, we ensure no prefix has more closing than opening brackets by treating unlocked characters as '('; 
+
         if(s.length() % 2 != 0){
             return false;
         } 
@@ -14,7 +16,7 @@ class Solution {
 
             if(bal < 0) return false;
         }
-
+//in the second pass, we ensure no suffix has more opening than closing brackets by treating unlocked characters as ')'.
         bal = 0;
         for(int i=s.length()-1; i>=0;  i--){
             if(locked.charAt(i) == '0' || s.charAt(i) == ')'){
