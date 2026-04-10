5c450030ec64a27e9b8ae7fd4467e4b63a13704d
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-04-10 14:01:17 +0530

    Time: 0 ms (100%), Space: 42.2 MB (23.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,6 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
         int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
         dp[0] = 1; //for 0 stairs 1 way to reach it
         dp[1] = 1; //for 1 stairs 1 way to reach from 0 to 1
 
