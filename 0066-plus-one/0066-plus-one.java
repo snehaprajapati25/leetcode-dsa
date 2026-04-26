67fcc75961aeee68eeef50833884e56ad60a4c5b
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-04-26 11:43:49 +0530

    Time: 0 ms (100%), Space: 43.4 MB (43.56%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int j=0; int num = 0;
 
         for(int i=digits.length-1; i>=0; i--){
             //if digits[i] is less than 9 i.e last digit is less than 9
-            if(digits[i]<9){
+            if(digits[i] < 9){
                 digits[i]++;
                 return digits;
             }
 
-            //if it equals to 9
-            digits[i] = 0; 
+            digits[i] = 0;
         }
 
         //if all digits are 9 like 999 then no becomes 1000
-        int res[] = new int[digits.length+1];
-        res[0] = 1;
-        return res;
-    }
+        int ans[] = new int[digits.length+1];
+        ans[0] = 1;
 
+        return ans;  
+    }
 }
