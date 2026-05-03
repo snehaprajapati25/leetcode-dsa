faff86501c7d3fcb99cf9f3a719c6286aaa587c1
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-05-03 19:13:23 +0530

    Time: 5 ms (38.09%), Space: 45.6 MB (48.97%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -13,22 +13,25 @@ class Solution {
 
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int dp[][] = new int[n][n];
+        int lastRow[] = new int[n];
 
         //base case when it is at last row
         for(int j=0; j<n; j++){
-            dp[n-1][j] = triangle.get(n-1).get(j);
+            lastRow[j] = triangle.get(n-1).get(j);
         }
 
         for(int i=n-2; i>=0; i--){
+            int curRow[] = new int[n];
             for(int j=i; j>=0; j--){
-                int down = triangle.get(i).get(j) + dp[i+1][j];
-                int dg = triangle.get(i).get(j) + dp[i+1][j+1];
+                int down = triangle.get(i).get(j) + lastRow[j];
+                int dg = triangle.get(i).get(j) + lastRow[j+1];
 
-                dp[i][j] = Math.min(down, dg);
+                curRow[j] = Math.min(down, dg);
             }
+
+            lastRow = curRow;
         }
 
-        return dp[0][0];
+        return lastRow[0];
     }
 }
