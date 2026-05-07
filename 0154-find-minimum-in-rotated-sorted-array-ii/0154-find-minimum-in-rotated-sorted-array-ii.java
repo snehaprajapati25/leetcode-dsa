8929d2d987ab6475229aa211d63133b77276ca6c
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-05-07 20:06:21 +0530

    Time: 91 ms (3.99%), Space: 45.1 MB (7.9%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -9,7 +9,9 @@ class Solution {
             // because the minimum element is in the right half of the array
             if(arr[mid] > arr[high]){
                 low = mid+1;
-            }else if(arr[mid] < arr[high]) {//because the minimum element is in the left half of the array
+            }
+            //because the minimum element is in the left half of the array
+            else if(arr[mid] < arr[high]) {
                 high = mid;
             }else{
                 high--; //deal wth duplicatees
