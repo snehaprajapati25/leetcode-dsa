b517ed4ca8ae99f671d2892ad4511e6ab13f9529
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-05-12 23:57:24 +0530

    Time: 0 ms (100%), Space: 42.8 MB (40.43%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,24 +1,21 @@
 class Solution {
-    public int rob(int[] nums) {
-        int n = nums.length;
-        if(n == 1)return nums[0];
-        return Math.max(maxSum(nums, 0, n-1) , maxSum(nums, 1, n));
-    }
+    public int f(int[] nums, int si, int ei){
+        int dp[] = new int[nums.length];
+        dp[si] = nums[si];
 
-    public int maxSum(int[] nums, int si, int ei){
-        int curSum = 0;
-        int prev = nums[si];
-        int prev2 = 0;
+        for(int i=si+1; i<=ei; i++){
+            int notRob = dp[i-1];
+            int rob  = nums[i];
 
-        for(int i=si+1; i<ei; i++){
-            int take = nums[i] + prev2;
-            int notTake = 0 + prev;
+            if(i> si+1) rob += dp[i-2];
 
-            curSum = Math.max(take, notTake);
-            prev2 = prev;
-            prev = curSum;
+            dp[i] =Math.max(rob, notRob); 
         }
 
-        return prev;
+        return dp[ei];
+    }
+    public int rob(int[] nums) {
+        if(nums.length == 1)return nums[0];
+        return Math.max(f(nums, 0, nums.length-2),f(nums, 1, nums.length-1));
     }
 }
