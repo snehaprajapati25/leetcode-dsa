e8201c4d58743395593aea8bcf8037891426077a
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-05-22 23:23:29 +0530

    Time: 1 ms (13.55%), Space: 41.9 MB (80.56%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -14,9 +14,23 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
 
-        for(int row[]: dp){
-            Arrays.fill(row, -1);
+        // dp[0][0] = 1;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 && j==0 ){
+                    dp[i][j] = 1;
+                    // continue;
+                }else{
+                    int up=0, left=0;
+
+                    if(i>0)up = dp[i-1][j];
+                    if(j>0)left = dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+            }
         }
-        return f(m-1, n-1, dp);
+
+        return dp[m-1][n-1];
     }
 }
