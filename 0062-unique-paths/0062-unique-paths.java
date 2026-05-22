963f8299b99da82c73caf8e0e90dc83372cd9dc7
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-05-22 23:06:12 +0530

    Time: 0 ms (100%), Space: 42.5 MB (12.29%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,38 +1,22 @@
 class Solution {
-    //space optimisation
-    public int uniquePaths(int m, int n) {
-        int prev[] = new int[n];// prev row
+    public int f(int i, int j, int dp[][]){
+        if(i==0 && j==0)return 1;
 
-        for(int i=0; i<m; i++){
-            int temp[] = new int[n];//current row
-            for(int j=0; j<n; j++){
-                if(i==0 && j==0){
-                    temp[j] = 1; //current block will be 0
-                    continue;
-                }
+        if(i<0 || j<0)return 0;
 
-                int up = 0, left = 0;
-                if(i>0) up = prev[j];
-                if(j>0) left = temp[j-1];
+        if(dp[i][j] != -1)return dp[i][j];
 
-                temp[j] = up + left;
-            }
+        int up = f(i-1, j, dp);
+        int left = f(i, j-1, dp);
 
-            prev = temp;
-        }
-
-        return prev[n-1];
+        return dp[i][j] = up+left;
     }
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
 
-    // public int funct(int i, int j, int dp[][]){
-    //     if(i==0 && j==0){
-    //         return 1; //reached path return 1;
-    //     }
-
-    //     if(i<0 || j<0)return 0;
-
-    //     if(dp[i][j] != -1)return dp[i][j];
-
-    //     return dp[i][j] = funct(i-1,j, dp) + funct(i, j-1, dp);
-    // }
+        for(int row[]: dp){
+            Arrays.fill(row, -1);
+        }
+        return f(m-1, n-1, dp);
+    }
 }
