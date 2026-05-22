bfdc6c1b51c1510d8a8b70d5b3362096bf2b7708
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-05-22 23:28:56 +0530

    Time: 0 ms (100%), Space: 42.2 MB (46.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,36 +1,38 @@
 class Solution {
-    public int f(int i, int j, int dp[][]){
-        if(i==0 && j==0)return 1;
+    // public int f(int i, int j, int dp[][]){
+    //     if(i==0 && j==0)return 1;
 
-        if(i<0 || j<0)return 0;
+    //     if(i<0 || j<0)return 0;
 
-        if(dp[i][j] != -1)return dp[i][j];
+    //     if(dp[i][j] != -1)return dp[i][j];
 
-        int up = f(i-1, j, dp);
-        int left = f(i, j-1, dp);
+    //     int up = f(i-1, j, dp);
+    //     int left = f(i, j-1, dp);
 
-        return dp[i][j] = up+left;
-    }
+    //     return dp[i][j] = up+left;
+    // }
+
     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m][n];
+        int prev[] = new int[n];
 
         // dp[0][0] = 1;
 
         for(int i=0; i<m; i++){
+            int cur[] = new int[n];
             for(int j=0; j<n; j++){
                 if(i == 0 && j==0 ){
-                    dp[i][j] = 1;
-                    // continue;
-                }else{
+                    cur[j] = 1;
+                    continue;
+                }
                     int up=0, left=0;
 
-                    if(i>0)up = dp[i-1][j];
-                    if(j>0)left = dp[i][j-1];
-                    dp[i][j] = up+left;
-                }
+                    if(i>0)up = prev[j];
+                    if(j>0)left = cur[j-1];
+                    cur[j] = up+left;
             }
+            prev = cur;
         }
 
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 }
