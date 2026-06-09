0b95eabb649829e6f2ed631d671c9ef18836636a
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-06-09 06:18:16 +0530

    Time: 6 ms (93.57%), Space: 46.3 MB (81.37%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,22 +1,24 @@
 class Solution {
     public int lcs(String s1, String s2, int n, int m){
-        int dp[][] = new int[n+1][m+1];
+        int prev[] = new int[m+1];
 
-        for(int i=0; i<=n; i++)dp[i][0] = 0;
+        // for(int i=0; i<=n; i++)dp[i][0] = 0;
 
-        for(int j=0; j<=m; j++)dp[0][j] = 0;
+        for(int j=0; j<=m; j++)prev[j] = 0;
 
         for(int i=1; i<=n; i++){
+            int cur[] = new int[m+1];
             for(int j=1; j<=m; j++){
                 if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    cur[j] = 1 + prev[j-1];
                 }else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    cur[j] = Math.max(prev[j], cur[j-1]);
                 }
             }
+            prev = cur;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 
     public int minDistance(String word1, String word2) {
