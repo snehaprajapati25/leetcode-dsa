df3c2151e0bfd06586e9ef84a092b102a9086986
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-06-14 00:06:02 +0530

    Time: 21 ms (44.62%), Space: 94 MB (40.98%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -4,7 +4,7 @@ class Solution {
         int dp[][] = new int[n+1][5]; //4 -> transaction B, S, B, S
 
         for(int ind=n-1; ind>=0; ind--){
-                for(int transNo=3; transNo>=0; transNo--){
+                for(int transNo=3; transNo>=0; transNo--){ //transNo start from 3 as one 4(n==4) transition is already done value = 0;
                     if(transNo % 2 == 0){ //buy
                         dp[ind][transNo] = Math.max(-prices[ind] + dp[ind+1][transNo+1], 0 + dp[ind+1][transNo]);
                     }else{
